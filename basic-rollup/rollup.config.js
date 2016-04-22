347470cd8d5709b63fb5dfcdc0d2bbf9f1ee25e2
Patrik Nyblad <patrik.nyblad@gmail.com>
2016-04-22 22:10:55 +0200

    Add WIP basic rollup project

---
+++ basic-rollup/rollup.config.js
@@ -0,0 +1,26 @@
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import npm from 'rollup-plugin-npm';
+
+
+export default {
+	entry: 'src/main.js',
+	dest: 'dist/app.js',
+
+	format: 'iife',
+	plugins: [
+		nodeResolve({
+			jsnext: true,
+		}),
+		commonjs(),
+		babel({
+			presets: [ 'es2015-rollup' ],
+		}),
+		npm({
+			jsnext: true,
+			main: true,
+			browser: true
+		}),
+	],
+};
